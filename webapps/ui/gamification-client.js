1fe3608a5cb32e7d66a3a61f20ef965082195d70
Ana Hinojosa <ana.hinojosa@xbyorange.com>
2021-03-17 10:14:23 +0100

    v6 commit

--- webapps/ui/gamification-client.js
+++ webapps/ui/gamification-client.js
@@ -1,6 +1,6 @@
 function updateLeaderBoard() {
     $.ajax({
-        url: "http://localhost:8083/leaders"
+        url: "http://localhost:8082/leaders"
     }).then(function(data) {
         $('#leaderboard-body').empty();
         data.forEach(function(row) {
@@ -12,7 +12,7 @@ function updateLeaderBoard() {
 
 function updateStats(userId) {
     $.ajax({
-        url: "http://localhost:8083/stats?userId=" + userId,
+        url: "http://localhost:8082/stats?userId=" + userId,
         success: function(data) {
             $('#stats-div').show();
             $('#stats-user-id').empty().append(userId);
